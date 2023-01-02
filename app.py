b10e59e3e47ab56ca2b55edb944c9889449c1ead
Kaan Turan <kaanturan_kontakt@hotmail.com>
2023-01-02 22:18:20 +0100

    second test

--- app.py
+++ app.py
@@ -13,12 +13,11 @@
 
 #import des flask objektes
 from flask import Flask, render_template
+app = Flask(__name__)
 
-app = Flask(__name__) #hält den namen des python moduls
-
-@app.route('/') #wenn die webpage abgefragt wird dann startet die software von hier
-
-def main():
-    return render_template('index.html')
-
-
+@app.route('/')
+def home():
+   return render_template('index.html')
+   
+if __name__ == '__main__':
+   app.run()
