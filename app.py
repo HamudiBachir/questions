c8f379247a703d2cdb0f74b8f9c0d9db4efb6b31
Kaan Turan <kaanturan_kontakt@hotmail.com>
2023-01-02 20:26:03 +0100

    test1

---
+++ app.py
@@ -0,0 +1,20 @@
+#man muss bevor man flask run macht,
+#in den ordner über cd gehen, also in den webapp ordner unten in der konsole
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+@app.route('/')
+
+
+
+def index():
+    return redirect(url_for('foo_function'))
+@app.route('/fowo') #link erweiterung
+
+def foo_function():
+    return 'Hellow'
+@app.route('/bar')
+
+def bar_function():
+    return redirect('/foo') # Not recommended to give redirect() a hard-coded UR
+
