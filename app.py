e54fe3dff3c57aa61261fb6cc579bc677912cd05
Kaan Turan <kaanturan_kontakt@hotmail.com>
2023-01-02 22:35:42 +0100

    html und css verbinden

--- app.py
+++ app.py
@@ -13,11 +13,24 @@
 
 #import des flask objektes
 from flask import Flask, render_template
-app = Flask(__name__)
 
+#templates = ordner für html files
+#static_folder = ordner für css files
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+
+
+#========================[ SEITEN REDIRECTION BEGINNT AB HIER ]=============================
+#path zur index seite (mainpage)
 @app.route('/')
-def home():
-   return render_template('index.html')
-   
+def index():
+   return render_template('index.html') #muss bei templates untergeordnet sein
+
+#path zur zweiten seite
+@app.route('/test/')
+def test():
+   return render_template('index2.html')
+
+
+#ist wichtig damit das programm als standalone läuft (check), wenn nicht könnte zu fehlern führen
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
