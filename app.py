f67d5b9b9ee44c3049370a727ce710a86b07db83
Furki898 <118890860+Furki898@users.noreply.github.com>
2023-01-24 23:37:07 +0100

    Update app.py
    Erneuerung

--- app.py
+++ app.py
@@ -29,51 +29,63 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 
 #path to index page (mainpage)
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+def get_db_connection(): #verbindung zur datenbank herstellen
+    conn = sqlite3.connect('database.db') #datenbank datei
+    conn.row_factory = sqlite3.Row #gibt daten aus der datenbank wieder
     return conn
 
-
+#Abrufen von Informationen aus der datenbank
 def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
+    conn = get_db_connection() #Verbindung zur Datenbank herstellen
+    
+    #method returns a single record or None if no more rows are available.
+    #gibt aus der tabelle "posts" nur eine reihe mit der id = x
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close() #verbindung trennen
+
+    if post is None: #wenn es keinen eintrag unter "id -> x" gibt dann bricht er es ab
         abort(404)
-    return post
+
+    return post #falls ja dann gib folgenden post wieder
 
 
 @app.route('/') #/ -> main page
 def index():
-    conn = get_db_connection()
+    conn = get_db_connection() #datenbankverbindung herstellen
+
+    #fetches all the rows of a query result. It returns all the rows as a list of tuples. An empty list is returned if there is no record to fetch.
+    #gibt alle werte aus der tabelle posts wieder
     posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
+    conn.close() #db verbindung trennen
+
+    #index.html rendern, posts=posts heißt alle spalten und reihen aus der tabelle "posts" in der datenbank zur verfügung stellen
     return render_template('index.html', posts=posts)
 
 
-@app.route('/create/', methods=('GET', 'POST'))
+@app.route('/create/', methods=('GET', 'POST')) #werte zur datenbank hinzufügen
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
+    if request.method == 'POST': #wenn gewählte methode hinzufügen
+
+        title = request.form['title'] #werte die hinzugefügt werden
         content = request.form['content']
 
-        if not title:
+        if not title: #wenn nichts bei title eingegeben wurde
             flash('Title is required!')
-        elif not content:
+        elif not content: #wenn nichts bei content eingesetzt wurde
             flash('Content is required!')
-        else:
+        else: #wenn alles ausgefüllt wurde (in alle felder wurde etwas reingeschrieben)
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content)) #werte der datenbank hinzufügen
+            
             conn.commit()
             conn.close()
-            return redirect(url_for('index'))
+
+            return redirect(url_for('index')) #weiterleitung an url
 
     return render_template('create.html')
 
-
+#mehr oder weniger das gleiche wie oben
 @app.route('/<int:id>/edit/', methods=('GET', 'POST'))
 def edit(id):
     post = get_post(id)
@@ -99,7 +111,7 @@ def edit(id):
 
     return render_template('edit.html', post=post)
 
-@app.route('/<int:id>/delete/', methods=('POST',))
+@app.route('/<int:id>/delete/', methods=('POST',)) #löschung von daten aus der datenbank
 def delete(id):
     post = get_post(id)
     conn = get_db_connection()
